9fecf84d6de4efb0485b54a5f06508308525c637
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2019-04-16 23:40:52 +0700

    fixbug:  not find utils/db

---
+++ packages/atomicdex-app/app/utils/db/index.js
@@ -0,0 +1,29 @@
+// @flow
+import level from 'level';
+import getConfig from '../config';
+
+let db = null;
+const config = getConfig();
+
+export function open(id: string) {
+  if (db) return db;
+  db = level(`${config.get('paths.userDataDir')}/${id}`, {
+    valueEncoding: 'json'
+  });
+  window.db = db;
+  return db;
+}
+
+export function close() {
+  if (db) {
+    db.close();
+    db = null;
+    return true;
+  }
+  return false;
+}
+
+export function get() {
+  return db;
+}
+// /Users/lehoang/Library/Application\ Support/Electron/atomicapp
